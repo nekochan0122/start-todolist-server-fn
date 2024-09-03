19e7cef86dbb7519138dd1db7b24ef0d95ada563
NekoChan <aaron74327@gmail.com>
2024-09-03 15:04:53 -0700

    Update client.tsx

--- app/client.tsx
+++ app/client.tsx
@@ -3,12 +3,12 @@ import { hydrateRoot } from 'react-dom/client'
 
 import { createRouter } from './router'
 
-const router = createRouter()
-
 declare global {
   interface Window {
     readonly root: HTMLElement
   }
 }
+
+const router = createRouter()
 
 hydrateRoot(window.root, <StartClient router={router} />)
