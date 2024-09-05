c281a45e767347e361a5399acb7a7a02b004a742
NekoChan <aaron74327@gmail.com>
2024-09-05 16:10:23 -0700

    Update index.tsx

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,6 @@
 import { useMutation, useSuspenseQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
-import { useState } from 'react'
+import type { FormEvent } from 'react'
 
 import { createTodo, deleteTodo, getTodolistQueryOptions, updateTodo } from '~/api/todo'
 
@@ -14,8 +14,6 @@ export const Route = createFileRoute('/')({
 })
 
 function Home() {
-  const [name, setName] = useState('')
-
   const todolistQuery = useSuspenseQuery(
     getTodolistQueryOptions(),
   )
@@ -24,7 +22,6 @@ function Home() {
     mutationKey: ['createTodo'],
     mutationFn: createTodo,
     onSuccess: () => {
-      setName('')
       todolistQuery.refetch()
     },
   })
@@ -45,18 +42,26 @@ function Home() {
     },
   })
 
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    const form = e.currentTarget
+    const formData = new FormData(form)
+
+    const name = formData.get('name') as string
+    if (name.trim().length === 0) return
+
+    createTodoMutation.mutate({ name }, {
+      onSuccess: () => form.reset(),
+    })
+  }
+
   return (
     <div className='p-2'>
-      <div className='flex gap-2 mb-2'>
-        <input
-          type='text'
-          value={name}
-          onChange={(e) => setName(e.target.value)} className='border-black border'
-        />
-        <button onClick={() => createTodoMutation.mutate({ name })}>
-          ➕
-        </button>
-      </div>
+      <form onSubmit={handleSubmit} className='flex gap-2 mb-2'>
+        <input name='name' type='text' className='border' />
+        <button type='submit'>➕</button>
+      </form>
 
       <div className='space-y-1'>
         {todolistQuery.data.map((todo) => (
