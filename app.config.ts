4b99a44379ed4ac96cc4c371f110d5cacde8d26d
NekoChan <aaron74327@gmail.com>
2024-09-06 05:36:29 -0700

    enable autoCodeSplitting

--- app.config.ts
+++ app.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   tsr: {
     appDirectory: 'app',
+    autoCodeSplitting: true,
     generatedRouteTree: 'app/route-tree.gen.ts',
   },
   vite: {
