323011ef5aa4feedb49f07d3bc0718a5c1563c34
NekoChan <aaron74327@gmail.com>
2024-09-18 22:51:49 -0700

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from '@tanstack/start/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  deployment: {
+    preset: 'node-server',
+  },
   tsr: {
     appDirectory: 'app',
     generatedRouteTree: 'app/route-tree.gen.ts',
   },
   vite: {
     plugins: () => [tsconfigPaths()],
-  },
-  deployment: {
-    preset: 'node-server',
   },
 })
